f0e69a38168a2860b1f56fdd2add2ad6558c64a9
Brian Hicks <brian@brianthicks.com>
2015-08-31 13:13:08 -0500

    package: extract FPM to separate struct

---
+++ hammer/fpm.go
@@ -0,0 +1,259 @@
+package hammer
+
+import (
+	"errors"
+	"github.com/Sirupsen/logrus"
+	shlex "github.com/anmitsu/go-shlex"
+	"io/ioutil"
+	"os/exec"
+	"path"
+)
+
+var (
+	ErrFieldRequired     = errors.New("field is required")
+	ErrInvalidScriptName = errors.New("invalid script name")
+)
+
+type FPM struct {
+	Package *Package
+
+	baseOpts []string
+	baseArgs []string
+}
+
+func NewFPM(p *Package) (*FPM, error) {
+	fpm := &FPM{
+		Package: p,
+	}
+
+	err := fpm.setBaseArgs()
+	if err != nil {
+		return nil, err
+	}
+
+	err = fpm.setBaseOpts()
+	if err != nil {
+		return nil, err
+	}
+
+	return fpm, nil
+}
+
+func (f *FPM) PackageFor(outType string) (string, error) {
+	// put args and opts all together
+	extra, err := f.extraArgs()
+	if err != nil {
+		return "", err
+	}
+
+	arguments := []string{}
+	arguments = append(arguments, f.baseOpts...)
+	arguments = append(arguments, f.optsForType(outType)...)
+	arguments = append(arguments, extra...)
+	arguments = append(arguments, f.baseArgs...)
+
+	f.Package.logger.WithField("args", arguments).Debug("running FPM with args")
+	fpm := exec.Command("fpm", arguments...)
+	out, err := fpm.CombinedOutput()
+
+	if fpm.ProcessState != nil {
+		f.Package.logger.WithFields(logrus.Fields{
+			"systemTime": fpm.ProcessState.SystemTime(),
+			"userTime":   fpm.ProcessState.UserTime(),
+			"success":    fpm.ProcessState.Success(),
+		}).Debug("package command exited")
+	} else {
+		f.Package.logger.Debug("package command exited")
+	}
+
+	return string(out), err
+}
+
+func (f *FPM) setBaseArgs() error {
+	args := []string{}
+
+	p := f.Package
+
+	// targets
+	for i, target := range p.Targets {
+		srcBuf, err := p.template.Render(target.Src)
+		if err != nil {
+			p.logger.WithField("index", i).Error("error templating target source name")
+			return err
+		}
+		src := srcBuf.String()
+
+		dest, err := p.template.Render(target.Dest)
+		if err != nil {
+			p.logger.WithField("index", i).Error("error templating target destination")
+			return err
+		}
+
+		// opt-in templating. We don't want to template *every* file because some
+		// things look like Go templates and aren't (see for example every other
+		// kind of mustache template)
+		if !target.Template {
+			args = append(args, src+"="+dest.String())
+		} else {
+			var content []byte
+			rawContent, err := ioutil.ReadFile(src)
+			if err != nil {
+				p.logger.WithFields(logrus.Fields{
+					"name":  src,
+					"error": err,
+				}).Error("error reading content")
+				return err
+			}
+
+			contentBuf, err := p.template.Render(string(rawContent))
+			if err != nil {
+				p.logger.WithFields(logrus.Fields{
+					"name":  src,
+					"error": err,
+				}).Error("error templating content")
+				return err
+			}
+			content = contentBuf.Bytes()
+
+			_, name := path.Split(src)
+			contentDest := path.Join(p.TargetRoot, name)
+			err = ioutil.WriteFile(contentDest, content, 0777)
+			if err != nil {
+				p.logger.WithFields(logrus.Fields{
+					"name":  src,
+					"error": err,
+				}).Error("error writing content")
+				return err
+			}
+
+			args = append(args, contentDest+"="+dest.String())
+		}
+	}
+
+	f.baseArgs = args
+	return nil
+}
+
+func (f *FPM) setBaseOpts() error {
+	opts := []string{
+		"-s", "dir",
+		"-p", f.Package.OutputRoot,
+	}
+
+	pkg := f.Package
+
+	// fields
+	type field struct {
+		Name     string
+		Value    string
+		Required bool
+	}
+	fields := []field{
+		{"name", pkg.Name, true},
+		{"version", pkg.Version, true},
+		{"iteration", pkg.Iteration, true},
+		{"epoch", pkg.Epoch, false},
+		{"license", pkg.License, false},
+		{"vendor", pkg.Vendor, false},
+		{"description", pkg.Description, false},
+		{"url", pkg.URL, false},
+		{"architecture", pkg.Architecture, false},
+	}
+
+	for _, field := range fields {
+		if field.Value == "" {
+			if field.Required {
+				pkg.logger.WithField("field", field.Name).Error(ErrFieldRequired)
+				return ErrFieldRequired
+			} else {
+				continue
+			}
+		}
+
+		templated, err := pkg.template.Render(field.Value)
+		if err != nil {
+			pkg.logger.WithFields(logrus.Fields{
+				"field": field.Name,
+				"error": err,
+			}).Error("failed to render field as template")
+			return err
+		}
+
+		opts = append(opts, "--"+field.Name, templated.String())
+	}
+
+	// dependencies
+	for _, rawDepend := range pkg.Depends {
+		depend, err := pkg.template.Render(rawDepend)
+		if err != nil {
+			pkg.logger.WithFields(logrus.Fields{
+				"error": err,
+				"raw":   rawDepend,
+			}).Error("failed to render dependency as template")
+		}
+		opts = append(opts, "--depends", depend.String())
+	}
+
+	// scripts
+	for name, location := range pkg.scriptLocations {
+		if name == "build" {
+			continue
+		}
+
+		if name != "before-install" && name != "after-install" && name != "before-remove" && name != "after-remove" && name != "before-upgrade" && name != "after-upgrade" {
+			pkg.logger.WithFields(logrus.Fields{
+				"script": name,
+			}).Error(ErrInvalidScriptName)
+		}
+
+		opts = append(opts, "--"+name, location)
+	}
+
+	// config files
+	for i, target := range pkg.Targets {
+		if !target.Config {
+			continue
+		}
+
+		dest, err := pkg.template.Render(target.Dest)
+		if err != nil {
+			pkg.logger.WithField("index", i).Error("error templating target destination")
+			return err
+		}
+
+		opts = append(opts, "--config-files", dest.String())
+	}
+
+	f.baseOpts = opts
+	return nil
+}
+
+func (f *FPM) optsForType(t string) []string {
+	opts := []string{
+		"-t", t,
+	}
+
+	switch t {
+	case "rpm":
+		opts = append(
+			opts,
+			"--rpm-auto-add-directories",
+		)
+		for _, exclude := range []string{"/etc/systemd", "/etc/systemd/system"} {
+			opts = append(
+				opts,
+				"--rpm-auto-add-exclude-directories", exclude,
+			)
+		}
+	}
+
+	return opts
+}
+
+func (f *FPM) extraArgs() ([]string, error) {
+	extra, err := shlex.Split(f.Package.ExtraArgs, true)
+	if err != nil {
+		f.Package.logger.WithField("error", err).Error("failed to parse extra args")
+	}
+	return extra, err
+}
--- hammer/package.go
+++ hammer/package.go
@@ -1,10 +1,8 @@
 package hammer
 
 import (
-	"errors"
 	"fmt"
 	"github.com/Sirupsen/logrus"
-	shlex "github.com/anmitsu/go-shlex"
 	"github.com/spf13/viper"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
@@ -13,10 +11,6 @@ import (
 	"path"
 	"runtime"
 	"strings"
-)
-
-var (
-	ErrInvalidScriptName = errors.New("invalid script name")
 )
 
 type Target struct {
@@ -55,6 +49,7 @@ type Package struct {
 	logger          *logrus.Entry
 	template        *Template
 	scriptLocations map[string]string
+	fpm             *FPM
 }
 
 func NewPackageFromYAML(content []byte) (*Package, error) {
@@ -146,6 +141,13 @@ func (p *Package) Setup() error {
 	}
 	p.scriptLocations = locations
 
+	// create an FPM instance
+	fpm, err := NewFPM(p)
+	if err != nil {
+		return err
+	}
+	p.fpm = fpm
+
 	return nil
 }
 
@@ -199,297 +201,18 @@ func (p *Package) Build() error {
 }
 
 func (p *Package) Package() error {
-	fpmArgs, err := p.fpmArgs()
-	if err != nil {
-		p.logger.WithField("error", err).Error("failed to get package args")
-		return err
-	}
-
-	// prepend extra args
-	extra, err := shlex.Split(p.ExtraArgs, true)
-	if err != nil {
-		p.logger.WithField("error", err).Error("failed to parse extra args")
-		return err
-	}
-	fpmArgs = append(extra, fpmArgs...)
-
 	for _, outType := range strings.Split(viper.GetString("type"), ",") {
-		out, err := p.FPM(fpmArgs, outType)
+		p.logger.WithField("type", outType).Info("packaging with FPM")
+		out, err := p.fpm.PackageFor(outType)
 		if err != nil {
 			p.logger.WithFields(logrus.Fields{
-				"error": err,
-				"out":   string(out),
+				"error":   err,
+				"out":     string(out),
+				"outType": outType,
 			}).Error("failed to package")
 			return err
 		}
 	}
 
-	p.logger.Info("finished packaging")
 	return nil
-}
-
-func (p *Package) FPM(args []string, pkgType string) ([]byte, error) {
-	logger := p.logger.WithField("type", pkgType)
-	logger.Info("packaging with FPM")
-
-	// prepend source and dest arguments
-	prefixArgs := []string{
-		"-s", "dir",
-		"-t", pkgType,
-		"-p", p.OutputRoot,
-	}
-	prefixArgs = append(prefixArgs, p.typeArgs(pkgType)...)
-	args = append(prefixArgs, args...)
-
-	logrus.WithField("args", args).Debug("running FPM with args")
-	fpm := exec.Command("fpm", args...)
-	out, err := fpm.CombinedOutput()
-
-	if err == nil && !fpm.ProcessState.Success() {
-		err = errors.New("package command exited with a non-zero exit code")
-	}
-
-	if fpm.ProcessState != nil {
-		logger.WithFields(logrus.Fields{
-			"systemTime": fpm.ProcessState.SystemTime(),
-			"userTime":   fpm.ProcessState.UserTime(),
-			"success":    fpm.ProcessState.Success(),
-		}).Debug("package command exited")
-	} else {
-		logger.Debug("package command exited")
-	}
-
-	return out, err
-}
-
-func (p *Package) typeArgs(t string) []string {
-	args := []string{}
-
-	// specific fixes for different output types
-	switch t {
-	case "rpm":
-		args = append(
-			args,
-			"--rpm-auto-add-directories", // TODO: document this somewhere
-		)
-	}
-
-	return args
-}
-
-func (p *Package) fpmArgs() ([]string, error) {
-	// create the arguments for FPM
-	args := []string{}
-
-	// name
-	name, err := p.template.Render(p.Name)
-	if err != nil {
-		p.logger.WithField("error", err).Error("failed to render name as template")
-		return args, err
-	}
-	args = append(args, "--name", name.String())
-
-	// version
-	version, err := p.template.Render(p.Version)
-	if err != nil {
-		p.logger.WithField("error", err).Error("failed to render version as template")
-		return args, err
-	}
-	args = append(args, "--version", version.String())
-
-	// iteration
-	iteration, err := p.template.Render(p.Iteration)
-	if err != nil {
-		p.logger.WithField("error", err).Error("failed to render iteration as template")
-		return args, err
-	}
-	args = append(args, "--iteration", iteration.String())
-
-	// epoch
-	if p.Epoch != "" {
-		epoch, err := p.template.Render(p.Epoch)
-		if err != nil {
-			p.logger.WithField("error", err).Error("failed to render epoch as template")
-			return args, err
-		}
-		args = append(args, "--epoch", epoch.String())
-	}
-
-	// license
-	if p.License != "" {
-		license, err := p.template.Render(p.License)
-		if err != nil {
-			p.logger.WithField("error", err).Error("failed to render license as template")
-			return args, err
-		}
-		args = append(args, "--license", license.String())
-	}
-
-	// vendor
-	if p.Vendor != "" {
-		vendor, err := p.template.Render(p.Vendor)
-		if err != nil {
-			p.logger.WithField("error", err).Error("failed to render vendor as template")
-			return args, err
-		}
-		args = append(args, "--vendor", vendor.String())
-	}
-
-	// description
-	if p.Description != "" {
-		description, err := p.template.Render(p.Description)
-		if err != nil {
-			p.logger.WithField("error", err).Error("failed to render description as template")
-			return args, err
-		}
-		args = append(args, "--description", description.String())
-	}
-
-	// url
-	if p.URL != "" {
-		url, err := p.template.Render(p.URL)
-		if err != nil {
-			p.logger.WithField("error", err).Error("failed to render url as template")
-			return args, err
-		}
-		args = append(args, "--url", url.String())
-	}
-
-	for _, rawDepend := range p.Depends {
-		depend, err := p.template.Render(rawDepend)
-		if err != nil {
-			p.logger.WithFields(logrus.Fields{
-				"error": err,
-				"raw":   rawDepend,
-			}).Error("failed to render dependency as template")
-		}
-		args = append(args, "--depends", depend.String())
-	}
-
-	// architecture
-	if p.Architecture != "" {
-		architecture, err := p.template.Render(p.Architecture)
-		if err != nil {
-			p.logger.WithField("error", err).Error("failed to render architecture as template")
-			return args, err
-		}
-		args = append(args, "--architecture", architecture.String())
-	}
-
-	scriptDir, err := ioutil.TempDir("", "hammer-scripts-"+p.Name)
-	if err != nil {
-		p.logger.WithField("error", err).Error("could not create script directory")
-		return args, err
-	}
-	p.ScriptRoot = scriptDir
-
-	for name, value := range p.Scripts {
-		if name == "build" {
-			continue
-		}
-		scriptLogger := p.logger.WithField("script", name)
-
-		// validate
-		if name != "before-install" && name != "after-install" && name != "before-remove" && name != "after-remove" && name != "before-upgrade" && name != "after-upgrade" {
-			scriptLogger.Error("invalid script name")
-			return args, ErrInvalidScriptName
-		}
-
-		content, err := p.template.Render(value)
-		if err != nil {
-			scriptLogger.WithField("error", err).Error("error in templating script")
-			return args, err
-		}
-
-		// write
-		loc := path.Join(scriptDir, name)
-		err = ioutil.WriteFile(loc, content.Bytes(), 0755)
-		if err != nil {
-			scriptLogger.WithField("error", err).Error("could not write script")
-			return args, err
-		}
-
-		scriptLogger.Debug("wrote script")
-		args = append(args, "--"+name, loc)
-	}
-
-	// config files
-	for i, target := range p.Targets {
-		if !target.Config {
-			continue
-		}
-
-		dest, err := p.template.Render(target.Dest)
-		if err != nil {
-			p.logger.WithField("index", i).Error("error templating target destination")
-			return args, err
-		}
-
-		args = append(args, "--config-files", dest.String())
-	}
-
-	// targets
-	targetDir, err := ioutil.TempDir("", "hammer-targets-"+p.Name)
-	if err != nil {
-		p.logger.WithField("error", err).Error("could not create target directory")
-		return args, err
-	}
-	p.TargetRoot = targetDir
-
-	for i, target := range p.Targets {
-		srcBuf, err := p.template.Render(target.Src)
-		if err != nil {
-			p.logger.WithField("index", i).Error("error templating target source name")
-			return args, err
-		}
-		src := srcBuf.String()
-
-		dest, err := p.template.Render(target.Dest)
-		if err != nil {
-			p.logger.WithField("index", i).Error("error templating target destination")
-			return args, err
-		}
-
-		// opt-in templating. We don't want to template *every* file because some
-		// things look like Go templates and aren't (see for example every other
-		// kind of mustache template)
-		if !target.Template {
-			args = append(args, src+"="+dest.String())
-		} else {
-			var content []byte
-			rawContent, err := ioutil.ReadFile(src)
-			if err != nil {
-				p.logger.WithFields(logrus.Fields{
-					"name":  src,
-					"error": err,
-				}).Error("error reading content")
-				return args, err
-			}
-
-			contentBuf, err := p.template.Render(string(rawContent))
-			if err != nil {
-				p.logger.WithFields(logrus.Fields{
-					"name":  src,
-					"error": err,
-				}).Error("error templating content")
-				return args, err
-			}
-			content = contentBuf.Bytes()
-
-			_, name := path.Split(src)
-			contentDest := path.Join(p.TargetRoot, name)
-			err = ioutil.WriteFile(contentDest, content, 0777)
-			if err != nil {
-				p.logger.WithFields(logrus.Fields{
-					"name":  src,
-					"error": err,
-				}).Error("error writing content")
-				return args, err
-			}
-
-			args = append(args, contentDest+"="+dest.String())
-		}
-	}
-
-	return args, nil
 }
