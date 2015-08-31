b4fef2e3931d9d820f43c217bba8d5b755adc9f1
Brian Hicks <brian@brianthicks.com>
2015-08-31 14:02:48 -0500

    template: add include function
    Fixes #7

--- hammer/template.go
+++ hammer/template.go
@@ -2,6 +2,8 @@ package hammer
 
 import (
 	"bytes"
+	"io/ioutil"
+	"path"
 	"text/template"
 )
 
@@ -11,19 +13,42 @@ type Template struct {
 }
 
 func NewTemplate(pkg *Package) *Template {
-	return &Template{
-		Package: pkg,
-		Funcs:   template.FuncMap{},
+	t := &Template{Package: pkg}
+
+	t.Funcs = template.FuncMap{
+		"include":   t.Include,
+		"specFile":  t.SpecFile,
+		"buildFile": t.BuildFile,
 	}
+
+	return t
 }
 
 func (t *Template) Render(in string) (bytes.Buffer, error) {
 	var buf bytes.Buffer
-	tmpl, err := template.New(in).Parse(in)
+	tmpl, err := template.New(in).Funcs(t.Funcs).Parse(in)
 	if err != nil {
 		return buf, err
 	}
 
-	err = tmpl.Funcs(t.Funcs).Execute(&buf, t.Package)
+	err = tmpl.Execute(&buf, t.Package)
 	return buf, err
+}
+
+// template functions
+
+func (t *Template) Include(path string) (string, error) {
+	out, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
+
+func (t *Template) SpecFile(name string) string {
+	return path.Join(t.Package.SpecRoot, name)
+}
+
+func (t *Template) BuildFile(name string) string {
+	return path.Join(t.Package.BuildRoot, name)
 }
